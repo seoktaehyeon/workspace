f2d4a375df0ebbcec446dfbf24236a587d451fb5
Will <seoktaehyeon@msn.com>
2019-12-03 14:14:00 +0800

    improve docker-registry-tool

--- tools/docker-registry-tool.sh
+++ tools/docker-registry-tool.sh
@@ -18,7 +18,7 @@ TAGS=""
 function log_debug
 {
     if [[ "${DEBUG}" == "debug" ]]; then
-        echo $@ 1>&2
+        echo "$@" 1>&2
     fi
     return 0
 }
@@ -39,18 +39,19 @@ function format_output
 function _get_token_url
 {
     url=${1?}
-    log_debug "${CURL} -v ${url}"
-    output=$(${CURL} -v "${url}" 2>&1)
+    log_debug "${CURL} --head ${url}"
+    output=$(${CURL} --head "${url}" 2>&1)
     echo "${output}" | grep -q '401' || {
         echo "${output}"
         exit 1
     }
     bearer=$(echo "${output}" | grep 'Bearer' | awk '{print $NF}')
-    bearer_info=$(format_output "${bearer}")
-    log_debug "${bearer_info}"
-    bearer_realm=$(echo "${bearer_info}" | grep 'realm' | awk -F '"' '{print $2}')
-    bearer_service=$(echo "${bearer_info}" | grep 'service' | awk -F '"' '{print $2}')
-    bearer_scope=$(echo "${bearer_info}" | grep 'scope' | awk -F '"' '{print $2}')
+    bearer_realm=$(echo "${bearer}" | awk -F 'realm=' '{print $2}' | awk -F '"' '{print $2}')
+    bearer_service=$(echo "${bearer}" | awk -F 'service=' '{print $2}' | awk -F '"' '{print $2}')
+    bearer_scope=$(echo "${bearer}" | awk -F 'scope=' '{print $2}' | awk -F '"' '{print $2}')
+    log_debug "REALM: ${bearer_realm}"
+    log_debug "SERVICE: ${bearer_service}"
+    log_debug "SCOPE: ${bearer_scope}"
     echo "${bearer_realm}?service=${bearer_service}&scope=${bearer_scope}"
     log_debug "Succeed to get bearer info"
     return 0
@@ -100,8 +101,7 @@ function get_repositories
 # Usage: get_tags <repo_name>
 function get_tags
 {
-#    repo_name=$(echo "$1" | awk -F '"' '{print $2}')
-    repo_name=$1
+    repo_name=${1?}
     url="${REGISTRY_HOST}/v2/${repo_name}/tags/list"
     echo -e "\nGet Tags: ${url}"
     token=$(_get_token "${url}")
@@ -121,9 +121,8 @@ function get_tags
 # Usage: get_digest <repo_name> <tag>
 function get_digest
 {
-#    repo_name=$(echo "$1" | awk -F '"' '{print $2}')
-    repo_name=$1
-    tag=$(echo "$2" | awk -F '"' '{print $2}')
+    repo_name=${1?}
+    tag=${2?}
     url="${REGISTRY_HOST}/v2/${repo_name}/manifests/${tag}"
     token=$(_get_token "${url}")
     log_debug "${CURL} -v -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -H 'Authorization: Bearer ${token}' '${url}'"
@@ -133,7 +132,9 @@ function get_digest
         exit 1
     }
     log_debug "200 OK"
-    echo "${output}" | grep -i 'Docker-Content-Digest' | awk '{print $NF}' | awk -F '\r' '{print $1}'
+    digest=$(echo "${output}" | grep -i 'Docker-Content-Digest' | awk '{print $NF}' | awk -F '\r' '{print $1}')
+    log_debug "DIGEST: ${digest}"
+    echo "${digest}"
     return 0
 }
 
@@ -143,6 +144,7 @@ function delete_tag
     url="${REGISTRY_HOST}/v2/${repo_name}/manifests/${1}"
     token=$(_get_token "${url}")
     log_debug "${CURL} -v -X DELETE -H 'Authorization: Bearer ${token}' '${url}'"
+    exit 0
     output=$(${CURL} -v -X DELETE -H "Authorization: Bearer ${token}" "${url}" 2>&1)
     echo "${output}" | grep -q '202' || {
         echo "${output}"
@@ -163,8 +165,9 @@ do
     do
         tag_count=$(( $tag_count + 1 ))
         (( $tag_count < 6 )) && continue
-        echo "more than 5" && break
-#        delete_tag $(get_digest ${repo} ${tag})
+        echo "more than 5"
+        digest=$(get_digest "${repo}" "${tag}")
+        echo "Fake delete it" && continue
+        delete_tag "${digest}"
     done
-#    break
 done
