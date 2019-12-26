498338a556eb82af40f8ef93e93b66f1a870fec6
Will <seoktaehyeon@msn.com>
2019-12-26 18:19:24 +0800

    update for config-vm-host.sh

--- tools/config-vm-host.sh
+++ tools/config-vm-host.sh
@@ -4,14 +4,26 @@
 _OS=''
 cat /etc/*release | grep -q CentOS && _OS='centos'
 cat /etc/*release | grep -q Ubuntu && _OS='ubuntu'
-
-echo "== Config network =="
+[[ "${_OS}" == "" ]] && {
+    echo "Unknown OS"
+    cat /etc/*release
+    exit 1
+}
 _IP=${1?}
 _NETMASK=${2-"255.255.0.0"}
-_GATEWAY=${3-"$(echo $_IP | awk -F '.' '{print $1"."$2}".0.1"')"}
+_DEFAULT_GATEWAY=$(echo ${_IP} | awk -F '.' '{print $1"."$2".0.1"}')
+_GATEWAY=${3-"${_DEFAULT_GATEWAY}"}
 _DNS1=${4-"192.168.1.1"}
 _DNS2=${5-"223.5.5.5"}
-echo "${_OS}" | grep -qE 'centos' && { 
+_HOSTNAME=${6-""}
+ping -c 4 ${_IP}
+(( $? == 0 )) && {
+    echo "${_IP} has been used, need change another one"
+    exit 1
+}
+
+echo "== Config network =="
+echo "${_OS}" | grep -qE 'centos' && {
     _IFCFG="/etc/sysconfig/network-scripts/$(ls /etc/sysconfig/network-scripts/ | grep 'ifcfg-' | grep -v 'ifcfg-lo')"
 
     echo ">> Enable ONBOOT"
@@ -60,59 +72,50 @@ echo "${_OS}" | grep -qE 'centos' && {
         echo "DNS2=${_DNS2}" >> ${_IFCFG}
     fi
 }
-echo "${_OS}" | grep -qE 'centos' && { 
-    _IFCFG="/etc/sysconfig/network-scripts/$(ls /etc/sysconfig/network-scripts/ | grep 'ifcfg-' | grep -v 'ifcfg-lo')"
+echo "${_OS}" | grep -qE 'ubuntu' && {
+    _IFCFG="/etc/network/interfaces"
 
-    echo ">> Enable ONBOOT"
-    sed -i 's/ONBOOT=.*/ONBOOT=yes/' ${_IFCFG}
+#    echo ">> Enable ONBOOT"
+#    sed -i 's/ONBOOT=.*/ONBOOT=yes/' ${_IFCFG}
 
-    echo ">> Set BOOTPROTO"
-    sed -i 's/BOOTPROTO=.*/BOOTPROTO=static/' ${_IFCFG}
+#    echo ">> Set BOOTPROTO"
+#    sed -i 's/BOOTPROTO=.*/BOOTPROTO=static/' ${_IFCFG}
 
     echo ">> Config ip address"
-    grep -q 'IPADDR=' ${_IFCFG}
+    grep -q 'address ' ${_IFCFG}
     if (( $? == 0 )); then
-        sed -i "s/IPADDR=.*/IPADDR=${_IP}/" ${_IFCFG}
+        sed -i "s/address .*/address ${_IP}/" ${_IFCFG}
     else
-        echo "IPADDR=${_IP}" >> ${_IFCFG}
+        echo "address ${_IP}" >> ${_IFCFG}
     fi
 
     echo ">> Config netmask"
-    grep -q 'NETMASK=' ${_IFCFG}
+    grep -q 'netmask ' ${_IFCFG}
     if (( $? == 0 )); then
-        sed -i "s/NETMASK=.*/NETMASK=${_NETMASK}/" ${_IFCFG}
+        sed -i "s/netmask .*/netmask ${_NETMASK}/" ${_IFCFG}
     else
-        echo "NETMASK=${_NETMASK}" >> ${_IFCFG}
+        echo "netmask ${_NETMASK}" >> ${_IFCFG}
     fi
 
     echo ">> Config gateway"
-    grep -q 'GATEWAY=' ${_IFCFG}
+    grep -q 'gateway ' ${_IFCFG}
     if (( $? == 0 )); then
-        sed -i "s/GATEWAY=.*/GATEWAY=${_GATEWAY}/" ${_IFCFG}
+        sed -i "s/gateway .*/gateway ${_GATEWAY}/" ${_IFCFG}
     else
-        echo "GATEWAY=${_GATEWAY}" >> ${_IFCFG}
+        echo "gateway ${_GATEWAY}" >> ${_IFCFG}
     fi
 
-    echo ">> Config dns1"
-    grep -q 'DNS1=' ${_IFCFG}
+    echo ">> Config dns-servers"
+    grep -q 'dns-servers ' ${_IFCFG}
     if (( $? == 0 )); then
-        sed -i "s/DNS1=.*/DNS1=${_DNS1}/" ${_IFCFG}
+        sed -i "s/dns-servers .*/dns-servers ${_DNS1} ${_DNS2}/" ${_IFCFG}
     else
-        echo "DNS1=${_DNS1}" >> ${_IFCFG}
-    fi
-
-    echo ">> Config dns2"
-    grep -q 'DNS2=' ${_IFCFG}
-    if (( $? == 0 )); then
-        sed -i "s/DNS2=.*/DNS2=${_DNS2}/" ${_IFCFG}
-    else
-        echo "DNS2=${_DNS2}" >> ${_IFCFG}
+        echo "dns-servers ${_DNS1} ${_DNS2}" >> ${_IFCFG}
     fi
 }
 
 echo "== Modify hostname =="
-_HOSTNAME=${6-""}
-if [[ "${_HOSTNAME}" == "" ]] || {
+[[ "${_HOSTNAME}" == "" ]] || {
     echo "${HOSTNAME}" > /etc/hostname
 }
 
@@ -131,27 +134,6 @@ echo "${_OS}" | grep -qE 'ubuntu' && {
 }
 
 echo "== Reboot VM =="
-reboot
-
-echo "== Modify hostname =="
-_HOSTNAME=${6-""}
-if [[ "${_HOSTNAME}" == "" ]] || {
-    echo "${HOSTNAME}" > /etc/hostname
-}
-
-echo "== Disable SeLinux =="
-which getenforce && setenforce 0
-[[ -f "/etc/selinux/config" ]] && sed -i 's/^SELINUX=.*/SELINUX=disable/' /etc/selinux/config 
-
-echo "== Disable Firewall =="
-echo "${_OS}" | grep -qE 'centos' && { 
-    systemctl stop firewalld
-    systemctl disable firewalld
-}
-echo "${_OS}" | grep -qE 'ubuntu' && { 
-    systemctl stop ufw 
-    systemctl disable ufw
-}
-
-echo "== Reboot VM =="
+echo "Reboot after 5 seconds"
+sleep 5
 reboot
